a854603bc1a77177df21339d2d121d60d73c60fd
Sri Lakshmi Narasimha Settipalli <narasimhas9490@gmail.com>
2024-05-26 06:48:01 +0530

    CRON for 5min

--- service.js
+++ service.js
@@ -16,7 +16,7 @@ if (!admin.apps.length) {
 const db = admin.database();
 const messaging = admin.messaging();
 
-cron.schedule('10 1 * * *', () => {
+cron.schedule('*/5 * * * *', () => {
   console.log("Cron job started at 01:10 AM Asia/Kolkata timezone");
   sendNotification();
 }, {
